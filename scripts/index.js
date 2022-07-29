37abb478d8676089db7ccfcd0a6f2cac5725ddca
Natasha Ivanova <terehinann@gmail.com>
2022-07-29 13:49:28 +0300

    feat: add function open/close

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,7 @@ const popupModal = document.querySelector('.popup_type_modal');
 const modalSrc = popupModal.querySelector('.popup__pic');
 const modalTitle = popupModal.querySelector('.popup__pic-caption');
 const closeModalPhoto = document.querySelector('#modalClose');
+
 //form add photo
 function handlePhotoFormSubmit (evt) {
   evt.preventDefault();
@@ -81,36 +82,48 @@ function handlerFormSubmit (evt) {
   closePopup();
 }
 
-function openPopup() {
-  popupProfile.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;
+//  Почему отрабатывает nameInput.value = profileName.textContent?
+// function openPopup() {
+//   popupProfile.classList.add('popup_opened');
+//   nameInput.value = profileName.textContent;
+//   descriptionInput.value = profileDescription.textContent;
+// }
+// function showPopupPhoto() {
+//   popupPhoto.classList.add('popup_type_photo_visible');
+// }
+
+function openPopup(popupName) {
+  popupName.classList.add('popup_opened');
+}
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
 }
 
-function closePopup() {
-  popupProfile.classList.remove('popup_opened');
-}
-
-function showPopupPhoto() {
-  popupPhoto.classList.add('popup_type_photo_visible');
-}
-
-function closePopupPhoto() {
-  popupPhoto.classList.remove('popup_type_photo_visible');
-}
-
+// function closePopup() {
+//   popupProfile.classList.remove('popup_opened');
+// }
+// function closePopupPhoto() {
+//   popupPhoto.classList.remove('popup_type_photo_visible');
+// }
 function closeModal() {
   popupModal.classList.remove('popup_type_modal_add');
 }
 
 //слушатели
 formElement.addEventListener('submit', handlerFormSubmit);
-closeButton.addEventListener('click', closePopup);
-editButton.addEventListener('click', openPopup);
+// closeButton.addEventListener('click', closePopup);
+closeButton.addEventListener('click', () => { closePopup(popupProfile); });
+
+// editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', () => { openPopup(popupProfile); });
+// addButton.addEventListener('click', showPopupPhoto);
+addButton.addEventListener('click', () => { openPopup(popupPhoto); });
 
 formPhoto.addEventListener('submit', handlePhotoFormSubmit);
 showPhoto.addEventListener('click', addPhoto);
-addButton.addEventListener('click', showPopupPhoto);
-closePhoto.addEventListener('click', closePopupPhoto);
 
-closeModalPhoto.addEventListener('click', closeModal);
+// closePhoto.addEventListener('click', closePopupPhoto);
+closePhoto.addEventListener('click', () => { closePopup(popupPhoto); });
+
+// closeModalPhoto.addEventListener('click', closeModal);
+closeModalPhoto.addEventListener('click', () => { closePopup(popupModal); });
