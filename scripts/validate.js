42829a1da5977d6105b789c35e7fc744d94f436c
Natasha Ivanova <terehinann@gmail.com>
2022-08-04 00:04:30 +0300

    feat:add validation

---
+++ scripts/validate.js
@@ -0,0 +1,62 @@
+function handleClickOverlay(evt) {
+  const activePopup = document.querySelector('.popup_opened');
+  if (activePopup && evt.target === activePopup) {
+    closePopup(activePopup);
+  }
+}
+const hasInvalidInput = (inputList) => {
+  return inputList.some(input => !input.validity.valid)
+};
+
+const toggleButtonState = (inputList, buttonElement, params) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(params.inactiveButtonSelector);
+  } else {
+    buttonElement.classList.remove(params.inactiveButtonSelector);
+  }
+};
+
+const setEventListeners = (formElement, params) => {
+  const inputList = Array.from(formElement.querySelectorAll(params.inputSelector));
+  const buttonElement = formElement.querySelector(params.submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    inputElement.setCustomValidity('');
+    inputElement.addEventListener('input', () => {
+      toggleButtonState(inputList, buttonElement, params);
+      isValid(formElement, inputElement, params)
+    });
+  });
+}
+
+const isValid = (formElement, inputElement, params) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, params);
+  } else {
+    hideInputError(formElement, inputElement, params);
+  }
+}
+
+const enableValidation = (params) => {
+  const formList = Array.from(document.querySelectorAll(params.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, params);
+  });
+};
+
+const showInputError = (formElement, inputElement, errorMessage, params) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id} + ${params.errorSelector}`);
+  inputElement.classList.add(params.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(params.errorActiveClass);
+};
+
+const hideInputError = (formElement, inputElement, params) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id} + ${params.errorSelector}`);
+  inputElement.classList.remove(params.inputErrorClass);
+  errorElement.classList.remove(params.errorActiveClass);
+  errorElement.textContent = '';
+};
